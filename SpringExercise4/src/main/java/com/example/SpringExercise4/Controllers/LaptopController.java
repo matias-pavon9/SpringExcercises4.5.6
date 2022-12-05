2a8d4a6a1fbf0ac48dcfc823ef6d8bf3445dea83
Matias Pavón <matiaspavon5@hotmail.com>
2022-12-05 10:23:46 -0300

    ---

--- SpringExercise4/src/main/java/com/example/SpringExercise4/Controllers/LaptopController.java
+++ SpringExercise4/src/main/java/com/example/SpringExercise4/Controllers/LaptopController.java
@@ -2,13 +2,12 @@ package com.example.SpringExercise4.Controllers;
 
 import com.example.SpringExercise4.Entities.Laptop;
 import com.example.SpringExercise4.Repositories.LaptopRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,7 +15,8 @@ import java.util.List;
 public class LaptopController {
 
     @Autowired
-    LaptopRepository laptopRepository;
+    private LaptopRepository laptopRepository;
+    private final Logger log = LoggerFactory.getLogger(LaptopController.class);
 
     @GetMapping("/laptop/all")
     public ResponseEntity<List<Laptop>> ListAllLaptops(){
@@ -25,8 +25,27 @@ public class LaptopController {
 
     @PostMapping("/laptop/create")
     public ResponseEntity<Laptop> CreateLaptop(@RequestBody Laptop laptop){
-        laptopRepository.save(laptop);
-        return ResponseEntity.status(HttpStatus.CREATED).body(laptop);
+        if (laptop.getId() == null) {
+            laptopRepository.save(laptop);
+            return ResponseEntity.status(HttpStatus.CREATED).body(laptop);
+        } else {
+            log.warn("You are trying to create a new Laptop with an Id");
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    @PutMapping("/laptop/update")
+    public ResponseEntity<Laptop> UpdateLaptop(@RequestBody Laptop laptop){
+        if (laptop.getId() == null){
+            log.warn("You are trying to update a Laptop without an Id");
+            return ResponseEntity.badRequest().build();
+        } else if (!laptopRepository.existsById(laptop.getId())){
+            log.warn("You are trying to update a non existing Laptop");
+            return ResponseEntity.notFound().build();
+        } else {
+            laptopRepository.save(laptop);
+            return ResponseEntity.ok(laptop);
+        }
     }
 
 }
